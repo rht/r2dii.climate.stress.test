bdb6aec87acd45fec3d5df8e2fcdb6d6d044b507
Mauro Lepore <maurolepore@gmail.com>
2021-11-06 14:28:12 -0600

    Polish

--- R/st_map.R
+++ R/st_map.R
@@ -1,24 +1,12 @@
 st_map <- function(data, asset_type, ..., quiet = TRUE) {
-  dots <- rlang::list2(...)
-  long <- purrr::keep(dots, ~length(.x) > 1L)
+  dots <- list2(...)
+  long <- keep(dots, ~length(.x) > 1L)
 
-  if (identical(length(long), 0L)) {
-    abort(c(
-      "Must privide one argument of `run_stress_test()` with multiple values.",
-      i = "Do you need to use `run_stress_test()` instead?"
-    ))
-  }
-
-  if (length(long) > 1L) {
-    abort(c(
-      "Must provide no more than one argument with multiple values.",
-      x = glue("Arguments with multiple values: {toString(names(long))}."),
-      i = "Did you forget to pick only one?"
-    ))
-  }
+  abort_if_no_argument_is_long(long)
+  abort_if_more_than_one_argument_is_long(long)
 
   dots1 <- dots[setdiff(names(dots), names(long))]
-  args1 <- rlang::list2(
+  args1 <- list2(
     data = data,
     asset_type = asset_type,
     !!!dots1,
@@ -34,6 +22,29 @@ st_map <- function(data, asset_type, ..., quiet = TRUE) {
     map(~exec(st, !!!.x)) %>%
     enframe(value = "st_result") %>%
     restructure_st_map()
+}
+
+abort_if_no_argument_is_long <- function(data) {
+  if (identical(length(data), 0L)) {
+    abort(c(
+      "Must privide one argument of `run_stress_test()` with multiple values.",
+      i = "Do you need to use `run_stress_test()` instead?"
+    ))
+  }
+
+  invisible(data)
+}
+
+abort_if_more_than_one_argument_is_long <- function(data) {
+  if (length(data) > 1L) {
+    abort(c(
+      "Must provide no more than one argument with multiple values.",
+      x = glue("Arguments with multiple values: {toString(names(data))}."),
+      i = "Did you forget to pick only one?"
+    ))
+  }
+
+  invisible(data)
 }
 
 restructure_st_map <- function(data) {
