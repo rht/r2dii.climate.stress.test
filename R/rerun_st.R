cbad98f710ead473d1293798933afcada43347e2
Mauro Lepore <maurolepore@gmail.com>
2021-11-06 15:00:42 -0600

    Reorganize files and functions

--- R/rerun_st.R
+++ R/rerun_st.R
@@ -1,3 +1,25 @@
+#' Sensitivity analysis: Rerun `run_stress_test()` multiple times
+#'
+#' This function performs the so called "sensitivity analysis". It runs
+#' [run_stress_test()] multiple times, iterating over multiple values of one and
+#' only one argument of [run_stress_test()].
+#'
+#' @param data Character vector of length 2. Paths to the directories
+#'   ST_INPUTS_MASTER and ST_TESTING_\{aset-type\}, e.g. ST_TESTING_BONDS,
+#'   respectively. These directories must have the files expected by the
+#'   stress-test project.
+#' @param asset_type An atomic character vector. Either "bonds", "equity", or
+#'   "loans".
+#' @param ... Arguments passed to [run_stress_test()].
+#' @param quiet Logical. Print non-condiiton messages to the console?
+#'
+#' @seealso [run_stress_test()].
+#'
+#' @return A data frame.
+#'
+#' @export
+#'
+#' @examples
 rerun_st <- function(data, asset_type, ..., quiet = TRUE) {
   dots <- list2(...)
   long <- keep(dots, ~ length(.x) > 1L) %>%
@@ -45,4 +67,31 @@ restructure_rerun_st <- function(data) {
     separate(.data$name, into = c("arg_name", "arg_value"), sep = "___") %>%
     unnest(cols = .data$st_result) %>%
     relocate(.data$st_type, .data$st_name)
+}
+
+st <- function(data, asset_type, ..., quiet = TRUE) {
+  vec_assert(data, character(), size = 2L)
+  data <- vec_set_names(data, envvar_names())
+  local_envvar(data)
+
+  control_verbosity <- ifelse(quiet, utils::capture.output, identity)
+  invisible(control_verbosity(run_stress_test(asset_type = asset_type, ...)))
+
+  outputs_path() %>%
+    dir_ls() %>%
+    map(~ read_csv(.x, show_col_types = FALSE)) %>%
+    enframe(value = "st_result") %>%
+    restructure_st()
+}
+
+restructure_st <- function(data) {
+  data %>%
+    mutate(name = path_file(name)) %>%
+    mutate(name = path_ext_remove(name)) %>%
+    mutate(name = sub("^stress_test_results_", "", name)) %>%
+    extract(name, into = c("st_type", "st_name"), "([^_]+)_(.*)")
+}
+
+envvar_names <- function() {
+  c("ST_DATA_PATH", "ST_PROJECT_FOLDER")
 }
